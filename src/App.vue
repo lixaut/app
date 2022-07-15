5635604846b012896a96fb08277239a9c9100d93
wangli <wanglixaut@qq.com>
2022-07-15 11:13:05 +0800

    路由组件的搭建

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <Header/>
+    <!-- 路由组件出口地方 -->
+    <router-view></router-view>
+    <Footer/>
   </div>
 </template>
 
 <script>
   import Header from './components/Header/index.vue'
+  import Footer from './components/Footer/index.vue'
+  
   export default {
     name: 'App',
     components: {
       Header,
+      Footer
     }
   }
 </script>
