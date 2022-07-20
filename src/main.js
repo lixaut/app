779465dc59a52b99d1e1cc6f9dc76ba1c0d8c27b
wangli <wanglixaut@qq.com>
2022-07-20 14:48:49 +0800

    三级联动

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 // 三级联动组件-全局组件
-import TypeNav from '@/pages/Home/TypeNav'
+import TypeNav from '@/components/TypeNav'
 
 // 第一个参数：全局组件的名字；第二个参数：哪一个组件
 Vue.component(TypeNav.name, TypeNav)
@@ -12,8 +12,8 @@ import router from '@/router'
 import store from '@/store'
 
 // 测试
-import {reqCategoryList} from '@/api'
-reqCategoryList()
+// import {reqCategoryList} from '@/api'
+// reqCategoryList()
 
 Vue.config.productionTip = false
 
--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,8 +1,31 @@
+
+import { reqCategoryList } from "@/api"
+
 // home模块的小仓库
-const state = {}
-const mutations = {}
-const actions = {}
+const state = {
+    // state中的数据默认数据不要乱写，根据接口文档初始化定义
+    categoryList: []
+}
+
+const mutations = {
+    CATEGORYLIST(state, categoryList) {
+        state.categoryList = categoryList
+    }
+}
+
+const actions = {
+    // 通过API里面的接口函数调用，向服务器发请求，获取服务器的数据
+    async categoryList({commit}) {
+        let result = await reqCategoryList()
+        if(result.code === 200) {
+            commit('CATEGORYLIST', result.data)
+        }
+        // console.log(result)
+    }
+}
+
 const getters = {}
+
 export default {
     state,
     mutations,
